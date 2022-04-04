8429218be2cfdb4c46b42984314818bed001ecc7
Mansurjon0608 <js1235gugl@gmail.com>
2022-04-04 18:43:38 +0500

    Google Map, Current Location, Permissions

--- app/src/main/java/com/mstar004/mapapp/MainActivity.kt
+++ app/src/main/java/com/mstar004/mapapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.mstar004.mapapp
 
+import android.Manifest
+import android.app.Dialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.Window
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+
+
 }
+
+
--- app/src/main/java/com/mstar004/mapapp/MapsFragment.kt
+++ app/src/main/java/com/mstar004/mapapp/MapsFragment.kt
@@ -1,13 +1,25 @@
 package com.mstar004.mapapp
 
+import android.Manifest
+import android.app.Dialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.location.Location
 import android.location.LocationManager
+import android.net.Uri
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
+import android.provider.Settings
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.app.ActivityCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -20,6 +32,7 @@ import com.google.android.gms.maps.model.MarkerOptions
 class MapsFragment : Fragment() {
 
     private lateinit var singleShotLocationProvider: SingleShotLocationProvider
+    private var dialogFineLocation: Dialog? = null
 
 
     override fun onCreateView(
@@ -27,6 +40,7 @@ class MapsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
+        checkPermission()
 
         return inflater.inflate(R.layout.fragment_maps, container, false)
     }
@@ -34,6 +48,7 @@ class MapsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
     }
@@ -58,6 +73,63 @@ class MapsFragment : Fragment() {
 
             })
 
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkPermission()
+    }
+
+    private fun checkPermission() {
+        dialogFineLocation?.dismiss()
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+            if (ActivityCompat.checkSelfPermission(requireContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(requireContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(requireContext(),
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED
+            ) {
+                dialogFineLocation = Dialog(requireContext(), androidx.appcompat.R.style.AlertDialog_AppCompat)
+                dialogFineLocation?.requestWindowFeature(Window.FEATURE_NO_TITLE)
+                dialogFineLocation?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                dialogFineLocation?.setContentView(R.layout.dialog_error_location)
+                dialogFineLocation?.setCanceledOnTouchOutside(false)
+                dialogFineLocation?.setCancelable(false)
+
+                dialogFineLocation?.findViewById<AppCompatTextView>(R.id.tvTurnLocation)
+                    ?.setOnClickListener {
+                        val viewIntent =
+                            Intent(ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse(
+                                "package:$ACTION_APPLICATION_DETAILS_SETTINGS"))
+                        startActivity(viewIntent)
+                    }
+                dialogFineLocation?.show()
+            }
+        } else if (ActivityCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) {
+            dialogFineLocation = Dialog(requireContext(), androidx.appcompat.R.style.AlertDialog_AppCompat)
+            dialogFineLocation?.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialogFineLocation?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            dialogFineLocation?.setContentView(R.layout.dialog_error_location)
+            dialogFineLocation?.setCanceledOnTouchOutside(false)
+            dialogFineLocation?.setCancelable(false)
+
+            dialogFineLocation?.findViewById<AppCompatTextView>(R.id.tvTurnLocation)
+                ?.setOnClickListener {
+
+                    val viewIntent =
+                        Intent(ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse(
+                            "package: $ACTION_APPLICATION_DETAILS_SETTINGS"))
+                    startActivity(viewIntent)
+                }
+            dialogFineLocation?.show()
+        }
 
     }
 }
